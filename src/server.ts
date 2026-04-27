f36bcd1f53fac7234be278febd11c40ae2230465
Alazar-dev <mealazartadesse@gmail.com>
2026-04-27 21:49:05 +0300

    feat: server setup

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from 'cors'
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI as string)
+    .then(() => console.log("MongoDB Connected!"))
+    .catch((err) => console.error(err));
+
+app.listen(process.env.PORT as string, () => console.log(`Listening on ${process.env.PORT}`));
