3b468b5a3be2586f51f86824d201398ff3bb9f44
Alazar Tadesse <69623057+Alazar-dev@users.noreply.github.com>
2026-05-14 00:15:24 +0300

    Update email transport configuration for Gmail

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,12 +1,15 @@
 import nodemailer from 'nodemailer'
 
 export const sendOtpEmail = async (email: string, otp: string) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
+     const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        family: 4,
         auth: {
             user: process.env.EMAIL_ID,
             pass: process.env.EMAIL_PASS,
-        }
+        },
     });
 
     transporter.sendMail({
